5ff053015da85a055f4991a7db062a568fa6cb18
Ezequiel-Moreira <99ezequiel90@gmail.com>
2019-03-19 15:33:55 +0000

    fixed main page added sparql queries now onto the options for format

--- routes/sparql.js
+++ routes/sparql.js
@@ -2,26 +2,34 @@ var express = require('express')
 var router = express.Router()
 var axios = require('axios')
 
-var endpoint = [
-    'http://localhost:7200/repositories/',
-    'https://dbpedia.org/sparql'
-]
+var endpoint = 'http://localhost:7200/repositories/'
 
 // ------------------Tratamento dos pedidos-------
 /* GET users listing. */
 router.get('/input/:id', function(req, res) {
-  res.render('getInput',{id: req.params.id})
+  res.render('getInput',{identity: req.params.id})
 })
 
 router.post('/input/:id', function(req, res){
+    //TODO: voltar a ver isto quando tiver vários tipos disponiveis
+    //obter query do texto
     var query = req.body.intext
     var encoded = encodeURIComponent(query)
 
-    axios.get(endpoint[0] + req.params.id + '?query=' + encoded)
+    //debug
+    console.log(query)
+    console.log(encoded)
+
+    //questionar o endpoint com a query
+    axios.get(endpoint + req.params.id + '?query=' + encoded)
         .then((result) => {
-            res.jsonp(result.data)
+            //se tivermos resultado, retornar para o ecra
+            console.log(result.data.results.bindings)
+            res.jsonp(result.data.results.bindings)
         }).catch((err) => {
-            console.log(err)
+            //caso contrário, mostrar erro
+            //console.log(err)
+            res.render('error',{message:"An error ocorred when trying to make the query",error:err})
         })
 })
 
